39809e249da61976ce53be38873267cf3cf4fc97
hamido <hamidabdellaoui55@gmail.com>
2021-12-27 01:46:53 +0100

    updating all files

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+from dash.html.Col import Col
 import dash_bootstrap_components as dbc
 from dash import Input, Output, State, dcc, html
 from dash_bootstrap_components._components.Navbar import Navbar
@@ -9,45 +10,101 @@ from dash_bootstrap_templates import load_figure_template
 
 import math
 
-millnames = ['',' Mille',' Million',' Milliard',' Mille milliards']
+millnames = ["", " Mille", " Million", " Milliard", "000 milliards"]
+
 
 def millify(n):
     n = float(n)
-    millidx = max(0,min(len(millnames)-1,
-                        int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+    millidx = max(
+        0,
+        min(
+            len(millnames) - 1, int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3))
+        ),
+    )
 
-    return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
-
+    return "{:.0f}{}".format(n / 10 ** (3 * millidx), millnames[millidx])
 
 
 load_figure_template(["CYBORG", "QUARTZ"])
 
 
-### importing Data and extracting 
-Export_total = pd.read_csv('Data/Export_total.csv')
-new_df = pd.read_csv('Data/new_df.csv')
-utilisation_import=Export_total[(Export_total["Libellé du flux"]=="Importations CAF")].groupby(["Libellé du groupement d'utilisation"]).sum().sort_values('Total dh', ascending=False)
-utilisation_export=Export_total[(Export_total["Libellé du flux"]=="Exportations FAB")].groupby(["Libellé du groupement d'utilisation"]).sum().sort_values('Total dh', ascending=False)
-pays_export=Export_total[(Export_total["Libellé du flux"]=="Exportations FAB")].groupby(["Libellé du pays"]).sum().sort_values('Total dh', ascending=False)
-pays_import=Export_total[(Export_total["Libellé du flux"]=="Importations CAF")].groupby(["Libellé du pays"]).sum().sort_values('Total dh', ascending=False)
-continent_import=Export_total[(Export_total["Libellé du flux"]=="Importations CAF")].groupby(["Continent"]).sum().sort_values('Total dh', ascending=False)
-continent_export=Export_total[(Export_total["Libellé du flux"]=="Exportations FAB")].groupby(["Continent"]).sum().sort_values('Total dh', ascending=False)
+### importing Data and extracting
+Export_total = pd.read_csv("Data/Export_total.csv")
+new_df = pd.read_csv("Data/new_df.csv")
+annual=pd.read_csv('Data/Annual.csv',sep=';',decimal=',')
 
-app = dash.Dash(suppress_callback_exceptions=True,
-                external_stylesheets=[dbc.themes.CYBORG, dbc.icons.FONT_AWESOME, "https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.bundle.min.js",
-                                      "https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.bundle.min.js"],
-                # these meta_tags ensure content is scaled correctly on different devices
-                # see: https://www.w3schools.com/css/css_rwd_viewport.asp for more
-                meta_tags=[
-                    {"name": "viewport", "content": "width=device-width, initial-scale=1"}
-                ],
-                )
+total_import= annual[(annual["Libellé du flux"]=="Importations CAF")].groupby(["Libellé du flux"]).sum().sort_values("Valeur DHS 2020", ascending=False)
+total_export= annual[(annual["Libellé du flux"]=="Exportations FAB")].groupby(["Libellé du flux"]).sum().sort_values("Valeur DHS 2020", ascending=False)
 
-# we use the Row and Col components to construct the sidebar header
-# it consists of a title, and a toggle, the latter is hidden on large screens
+utilisation_import = (
+    Export_total[(Export_total["Libellé du flux"] == "Importations CAF")]
+    .groupby(["Libellé du groupement d'utilisation"])
+    .sum()
+    .sort_values("Total dh", ascending=False)
+)
+utilisation_export = (
+    Export_total[(Export_total["Libellé du flux"] == "Exportations FAB")]
+    .groupby(["Libellé du groupement d'utilisation"])
+    .sum()
+    .sort_values("Total dh", ascending=False)
+)
+pays_export = (
+    annual[(annual["Libellé du flux"] == "Exportations FAB")]
+    .groupby(["Libellé du pays"])
+    .sum()
+    .sort_values("Valeur DHS 2020", ascending=False)
+)
+pays_import = (
+    annual[(annual["Libellé du flux"] == "Importations CAF")]
+    .groupby(["Libellé du pays"])
+    .sum()
+    .sort_values("Valeur DHS 2020", ascending=False)
+)
+continent_import = (
+    Export_total[(Export_total["Libellé du flux"] == "Importations CAF")]
+    .groupby(["Continent"])
+    .sum()
+    .sort_values("Total dh", ascending=False)
+)
+continent_export = (
+    Export_total[(Export_total["Libellé du flux"] == "Exportations FAB")]
+    .groupby(["Continent"])
+    .sum()
+    .sort_values("Total dh", ascending=False)
+)
+
+
+# some variables
+max_p_i = millify(pays_import['Valeur DHS 2020'][0])
+max_p_i_i = pays_import.index[0]
+
+max_p_e = millify(pays_export['Valeur DHS 2020'][0])
+max_p_e_i = pays_export.index[0]
+
+max_t_i= millify(total_import['Valeur DHS 2020'][0])
+max_t_i_i = total_import.index[0]
+
+max_t_e= millify(total_export['Valeur DHS 2020'][0])
+max_t_e_i = total_export.index[0]
+
+app = dash.Dash(
+    suppress_callback_exceptions=True,
+    external_stylesheets=[
+        dbc.themes.CYBORG,
+        dbc.icons.FONT_AWESOME,
+        "https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.bundle.min.js",
+        "https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.bundle.min.js",
+    ],
+    # these meta_tags ensure content is scaled correctly on different devices
+    # see: https://www.w3schools.com/css/css_rwd_viewport.asp for more
+    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
+)
+
+
 sidebar_header = dbc.Row(
     [
-        dbc.Col(html.H6("OCM", className="display-4")),
+        dbc.Col(html.H6("OEM", className="display-6 "),align="start",xs=8, md=8, lg=8, xl=8, xxl=8, sm=8),
+        dbc.Col(" ",xs=2, md=2, lg=2, xl=2, xxl=2, sm=2),
         dbc.Col(
             [
                 html.Button(
@@ -75,9 +132,9 @@ sidebar_header = dbc.Row(
             ],
             # the column containing the toggle will be only as wide as the
             # toggle, resulting in the toggle being right aligned
-            width="auto",
+            className="mb-3",
             # vertically align the toggle in the center
-            align="center",
+            align="end",xs=2, md=2, lg=2, xl=2, xxl=2, sm=2
         ),
     ]
 )
@@ -85,8 +142,6 @@ sidebar_header = dbc.Row(
 sidebar = html.Div(
     [
         sidebar_header,
-        # we wrap the horizontal rule and short blurb in a div that can be
-        # hidden on a small screen
         html.Div(
             [
                 html.Hr(),
@@ -112,111 +167,195 @@ sidebar = html.Div(
         ),
     ],
     id="sidebar",
+
 )
 
 
 def drawFigure():
-    df = pd.DataFrame({
-        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-        "Amount": [4, 1, 2, 2, 4, 5],
-        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]})
-    fig = px.bar(df, x="Fruit", y="Amount", color="City", color_continuous_scale=px.colors.sequential.Viridis)
-    fig.update_layout({'plot_bgcolor': 'rgba(138, 138, 138, 0)',
-                      'paper_bgcolor': 'rgba(138, 138, 138, 0)', },
-    font=dict(
-        family="Lato, monospace",
-        size=9,
-        color="#fff"
-    ))
-    return html.Div([
-            
-                html.Div([ html.Div("Title"),
+    df = pd.DataFrame(
+        {
+            "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+            "Amount": [4, 1, 2, 2, 4, 5],
+            "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+        }
+    )
+    fig = px.bar(
+        df,
+        x="Fruit",
+        y="Amount",
+        color="City",
+        color_continuous_scale=px.colors.sequential.Viridis,
+    )
+    fig.update_layout(
+        {
+            "plot_bgcolor": "rgba(138, 138, 138, 0)",
+            "paper_bgcolor": "rgba(138, 138, 138, 0)",
+        },
+        font=dict(family="Lato, monospace", size=9, color="#fff"),
+    )
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.Div("Title"),
                     dcc.Graph(
-                        id='example-graph',
-                        figure=fig, responsive=True,
-                        style={'width': '35vh', 'height': '35vh'},
+                        id="example-graph",
+                        figure=fig,
+                        responsive=True,
+                        style={"width": "auto", "height": "200px"},
                     ),
-                ], style={'textAlign': 'center'}, className="mycard")
-           
-        
-    ])
+                ],
+                style={"textAlign": "center"},
+                className="mycard",
+            )
+        ]
+    )
 
 
-def drawText():
-    return dbc.Row([
-        dbc.Row([
-            dbc.Col(dbc.Nav([html.P(max+" Mad"+ "KPI1_index")]),
-                    className="KPI", xs=12, sm=4, md=4, lg=4, xl=2),
-            dbc.Col(dbc.Nav([html.H2("Text")]),
-                    className="KPI", xs=12, sm=4, md=4, lg=4, xl=2),
-            dbc.Col(dbc.Nav([html.H2("Text")]),
-                    className="KPI", xs=12, sm=4, md=4, lg=4, xl=2),
-            dbc.Col(dbc.Nav([html.H2("Text")]),
-                    className="KPI", xs=12, sm=4, md=4, lg=4, xl=2),
-        ], className="justify-content-center", style={'textAlign': 'center'})
-    ], className="justify-content-end", id="mycollapse")
+def kpi1():
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.Div('Plus grand importateur',className='title'),
+                    html.Hr(),
+                    max_p_i + " Dh",
+                    html.Br(),
+                    max_p_i_i
+                    ],
+                style={"textAlign": "center"},
+                className="KPI",
+                )
+                ]
+    )
+
+
+def kpi2():
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.Div('Plus grand exportateur',className='title'),
+                    html.Hr(),
+                    max_p_e + " Dh",
+                    html.Br(),
+                    max_p_e_i
+                    ],
+                style={"textAlign": "center"},
+                className="KPI",
+                )
+                ]
+    )
+
+
+def kpi3():
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.Div('Total exportations 2020',className='title'),
+                    html.Hr(),
+                    max_t_e + " Dh",
+                    html.Br(),
+                    max_t_e_i
+                    ],
+                style={"textAlign": "center"},
+                className="KPI",
+                )
+                ]
+    )
+
+
+def kpi4():
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.Div('Total importations 2020',className='title'),
+                    html.Hr(),
+                    max_t_i + " Dh",
+                    html.Br(),
+                    max_t_i_i
+                    ],
+                style={"textAlign": "center"},
+                className="KPI",
+                )
+                ]
+    )
 
 
 content = html.Div(id="page-content")
 blank = html.Div(id="blank_output")
 
-row = html.Div([
-    dbc.Row(
-        [
-            dbc.Col("", xs=2, sm=2, md=2, lg=2, xl=2),
-            dbc.Col(html.H3("Bonjour"), xs=2, sm=2, md=2, lg=2, xl=2),
-            dbc.Col("", xs=2, sm=2, md=2, lg=2, xl=2),
-            dbc.Col("", xs=2, sm=2, md=2, lg=2, xl=2),
-            dbc.Col(html.Div(
+row = html.Div(
+    [
+        dbc.Row(
+            [
+                dbc.Col("", xs=2, sm=2, md=2, lg=2, xl=2),
+                dbc.Col(html.H6(""), xs=2, sm=2, md=2, lg=2, xl=2),
+                dbc.Col("", xs=2, sm=2, md=2, lg=2, xl=2),
+                dbc.Col("", xs=2, sm=2, md=2, lg=2, xl=2),
+                dbc.Col(
+                    html.Div(
+                        [
+                            html.Span(
+                                className="fa fa-sun mt-1",
+                            ),
+                            html.Span(
+                                [dbc.Switch(
+                                value=True,
+                                id="theme",
+                                className="ms-4 switcher_icon",),]
+                            ),
+                            
+                            html.Span(className="fa fa-moon mt-1"),
+                        ],
+                        className="justify-content-center switcher",
+                    ), xs=4, sm=4, md=4, lg=4, xl=4
+                ),
+            ]
+        ),
+        html.Br(),
+        html.Div(
+            [
+                dbc.Row(
                     [
-                        html.Span(className="fa fa-sun",),
-                        dbc.Switch(value=True, id="theme", className="",),
-                        html.Span(className="fa fa-moon"),
+                        dbc.Row(dbc.Col('Quelques metrics de 2020'),style={"textAlign": "center"},className="titre"),
+                        dbc.Col([kpi1()],xs=6, sm=6, md=6, lg=3, xl=2,
+                        ),
+                        dbc.Col([kpi2()],xs=6, sm=6, md=6, lg=3, xl=2,
+                        ),
+                        dbc.Col([kpi3()],xs=6, sm=6, md=6, lg=3, xl=2,
+                        ),
+                        dbc.Col([kpi4()],xs=6, sm=6, md=6, lg=3, xl=2,
+                        ),
                     ],
-                    className="d-flex p-2 justify-content-between",
-                    ), xs=2, sm=2, md=2, lg=2, xl=2),
-            dbc.Col("", xs=2, sm=2, md=2, lg=2, xl=2),
-        ]),
-
-
-    html.Br(),
-        html.Div([
-            dbc.Row([
-                drawText()
-            ], align='center'),
-            html.Br(),
-            dbc.Row([
-                dbc.Col([
-                    drawFigure()
-                ],  xs=12, sm=6, md=6, lg=3, xl=3),
-                dbc.Col([
-                    drawFigure()
-                ], xs=12, sm=6, md=6, lg=3, xl=3),
-                dbc.Col([
-                    drawFigure()
-                ], xs=12, sm=6, md=6, lg=3, xl=3),
-                dbc.Col([
-                    drawFigure()
-                ], xs=12, sm=6, md=6, lg=3, xl=3),
-            ], align='center'),
-            dbc.Row([
-                dbc.Col([
-                    drawFigure()
-                ],  xs=12, sm=12, md=12, lg=6, xl=6),
-                dbc.Col([
-                    drawFigure()
-                ], xs=12, sm=4, md=4, lg=3, xl=3),
-                dbc.Col([
-                    drawFigure()
-                ], xs=12, sm=4, md=4, lg=3, xl=3),
-                dbc.Col([
-                    drawFigure()
-                ], xs=12, sm=4, md=4, lg=3, xl=3),
-            ], align='center'),
-
-        ],className="bCard")
-    
-])
+                    id="",style={"textAlign": "center"},
+                    align="center",
+                    className='justify-content-center'
+                ),
+                html.Hr(),
+                dbc.Row(
+                    [
+                        dbc.Col([drawFigure()], xs=12, sm=6, md=6, lg=6, xl=3),
+                        dbc.Col([drawFigure()], xs=12, sm=6, md=6, lg=6, xl=3),
+                        dbc.Col([drawFigure()], xs=12, sm=6, md=6, lg=6, xl=3),
+                        dbc.Col([drawFigure()], xs=12, sm=6, md=6, lg=6, xl=3),
+                        dbc.Col([drawFigure()], xs=12, sm=12, md=12, lg=6, xl=6),
+                        dbc.Col([drawFigure()], xs=12, sm=4, md=4, lg=3, xl=3),
+                        dbc.Col([drawFigure()], xs=12, sm=4, md=4, lg=3, xl=3),
+                        dbc.Col([drawFigure()], xs=12, sm=4, md=4, lg=3, xl=3),
+                    ],
+                    id="",style={"textAlign": "center"},
+                    align="center",
+                    className='justify-content-center'
+                ),
+            ],
+            style={"textAlign": "center"},
+                    className='justify-content-center bCard'
+        ),
+    ]
+)
 
 
 app.layout = html.Div([dcc.Location(id="url"), sidebar, blank, content])
@@ -242,11 +381,17 @@ def render_page_content(pathname):
     if pathname == "/":
         return html.Div(row)
     elif pathname == "/page-1":
-        return [html.H2("HEY THERE !", style={'textAlign': 'center'}),
-                html.Hr(),
-                html.Div("cette dashbord traite les échanges d'importation et exportation entre le maroc et les autres pays", style={'textAlign': 'center'})]
+        return [
+            html.H2("HEY THERE !", style={"textAlign": "center"}),
+            html.Hr(),
+            html.Div(
+                "cette dashbord traite les échanges d'importation et exportation entre le maroc et les autres pays",
+                style={"textAlign": "center"},
+            ),
+            html.Iframe(srcDoc='assets\Data_prep.html')
+        ]
     elif pathname == "/page-2":
-        return html.H2("What are u looking for?!", style={'textAlign': 'center'})
+        return html.H2("What are u looking for?!", style={"textAlign": "center"})
     # If the user tries to reach a different page, return a 404 message
     return dbc.Jumbotron(
         [
@@ -287,10 +432,12 @@ def toggle_collapse(n, is_open):
 def toggle_collapse(n, is_open):
     if n:
         return not is_open
-    return  is_open
+    return is_open
 
 
 if __name__ == "__main__":
-    app.run_server(port=8888, debug=True,
-                   #host="0.0.0.0",
-                   )
+    app.run_server(
+        port=8080,
+        debug=True,
+        # host="0.0.0.0",
+    )
